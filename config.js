95c28c32e6f9aea05bfa50182318cab556d44b57
6b2w6jzsy2-web <6b2w6jzsy2@privaterelay.appleid.com>
2026-08-01 21:23:05 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.SITE_CONFIG = {
-  businessName: "10X Property Buyers",
-  phoneDisplay: "UPDATE PHONE NUMBER",
-  phoneLink: "",
-  whatsappNumber: "",
-  email: "UPDATE EMAIL ADDRESS",
-  serviceArea: "Sussex and the South East"
-};
